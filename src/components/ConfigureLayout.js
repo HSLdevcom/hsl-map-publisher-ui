5ea913b6a9ccb82c872b6dcc2ab8162412eb2839
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-06-20 09:49:24 +0300

    Make resizing work nicely.

--- src/components/ConfigureLayout.js
+++ src/components/ConfigureLayout.js
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { blue50 } from 'material-ui/styles/colors';
-import { computed, observable } from 'mobx';
+import { computed, observable, toJS } from 'mobx';
 import TemplateSelect from './TemplateSelect';
 import get from 'lodash/get';
 
@@ -26,19 +26,20 @@ const Area = styled.div`
   display: grid;
   grid-gap: 2rem;
   grid-template-columns: ${({ columns = '1fr 1fr 1fr' }) => columns};
-  writing-mode: ${({ direction = 'ltr' }) => (direction === 'rtl' ? 'bt-rl' : 'horizontal-tb')};
-  direction: ${({ direction = 'ltr' }) => direction};
   cursor: ${({ resizing = false }) => (resizing ? 'col-resize' : 'default')};
+  pointer-events: ${({ resizing = false }) => (resizing ? 'auto' : 'none')};
+  user-select: none;
 `;
 
 const AreaSlot = styled.div`
   border-radius: 1rem;
-  flex: none;
+  flex: 0 0 auto;
   border: 3px dashed white;
   position: relative;
   height: 15rem;
   overflow: hidden;
-  width: calc(100% + ${({ resizeValue = 0 }) => resizeValue}px);
+  position: relative;
+  transform: translateZ(0);
 
   svg {
     width: 100%;
@@ -55,6 +56,7 @@ const Handle = styled.div`
   height: 2rem;
   box-sizing: content-box;
   cursor: col-resize;
+  pointer-events: all;
 
   &:before {
     content: '';
@@ -76,6 +78,11 @@ const HandleRight = styled(Handle)`
 const Item = styled.div`
   color: red;
 `;
+
+function getSiblingIndex(index, direction) {
+  const siblingIdx = direction === 'left' ? index - 1 : index + 1;
+  return siblingIdx < 0 || siblingIdx > 2 ? 1 : siblingIdx;
+}
 
 @observer
 class ConfigureLayout extends Component {
@@ -141,32 +148,46 @@ class ConfigureLayout extends Component {
     const mouseX = e.clientX;
     const delta = direction === 'left' ? start - mouseX : mouseX - start;
 
-    // eslint-disable-next-line
-    image.resizing.value = this.imageCount > 1 || size === 1 ? Math.max(delta, 0) : delta;
+    if (size > 1 && delta < 0) {
+      image.resizing.value = Math.min(delta, -10);
+    } else {
+      image.resizing.value = Math.max(delta, 0);
+    }
   };
 
   onHandleMouseUp = e => {
-    if (this.currentlyResizingImage && this.currentlyResizingImage.resizing) {
+    e.stopPropagation();
+
+    if (this.currentlyResizingImage) {
       const image = this.currentlyResizingImage;
       const { value, direction } = image.resizing;
-      const { width } = e.target.getBoundingClientRect(); // Measure Area el
-
-      const index = this.visibleImages.indexOf(image);
 
       const slotCount = this.visibleImages.reduce((count, img) => count + img.size, 0);
-      const slotWidth = width / slotCount;
-      const affectedSiblingIdx = direction === 'left' && index > 0 ? index - 1 : index + 1;
+      // TODO: Measure slot width
+      const slotWidth = 880 / slotCount;
+      const actionAreaWidth = slotWidth / 2;
 
-      if (this.imageCount > 1 && value > slotWidth) {
+      if (this.imageCount > 1 && value > actionAreaWidth) {
+        const index = this.visibleImages.indexOf(image);
+        const affectedSiblingIdx = getSiblingIndex(index, direction);
+
         image.size = image.size + 1;
-        this.visibleImages[affectedSiblingIdx].size = 0;
-      } else if (value < -slotWidth && image.size > 1) {
-        image.size = image.size - 1;
         const siblingSize = get(this, `visibleImages[${affectedSiblingIdx}].size`, 0);
-        this.visibleImages[affectedSiblingIdx].size = siblingSize + 1;
+        this.visibleImages[affectedSiblingIdx].size = siblingSize - 1;
+      } else if (value < -actionAreaWidth && image.size > 1) {
+        const absoluteIndex = get(this, 'currentTemplate.images', []).indexOf(image);
+        const affectedSiblingIdx = getSiblingIndex(absoluteIndex, direction);
+
+        image.size = image.size - 1;
+        const siblingSize = get(this, `currentTemplate.images[${affectedSiblingIdx}].size`, 0);
+        this.currentTemplate.images[affectedSiblingIdx].size = siblingSize + 1;
       }
     }
 
+    this.resetResize();
+  };
+
+  resetResize = () => {
     this.currentTemplate.images.forEach(img => {
       // eslint-disable-next-line
       img.resizing = null;
@@ -176,53 +197,58 @@ class ConfigureLayout extends Component {
   };
 
   render() {
-    const { templates, selectedTemplate, onSelectTemplate } = this.props;
-    const resizeDirection = get(this, 'currentlyResizingImage.resizing.direction', 'right');
-
-    const direction = resizeDirection === 'left' ? 'rtl' : 'ltr';
+    const { templates, onSelectTemplate } = this.props;
+    /*const resizeDirection = get(this, 'currentlyResizingImage.resizing.direction', 'right');
+    const direction = this.imageCount === 1 && resizeDirection === 'left' ? 'rtl' : 'ltr';*/
 
     return (
       <Root>
         <h2>Templates</h2>
         <TemplateSelect
           templates={templates}
-          selectedTemplate={selectedTemplate}
+          selectedTemplate={get(this, 'currentTemplate.id', null)}
           onChange={onSelectTemplate}
         />
         <AreaContainer>
           <h3>Footer</h3>
           <Area
-            direction={direction}
             resizing={!!this.currentlyResizingImage}
             onMouseUp={this.onHandleMouseUp}
             onMouseMove={this.onHandleMouseMove}
             columns={this.currentTemplateColumns}>
-            {this.currentTemplate &&
-              this.currentTemplate.images.map((image, idx, all) => {
-                if (image.size === 0) {
-                  return null;
-                }
+            {this.visibleImages.map((image, idx, all) => {
+              if (image.size === 0) {
+                return null;
+              }
 
-                const isFirst = idx === 0;
-                const isLast = idx >= all.length - 1;
-                const { resizing = null } = image;
+              const isFirst = idx === 0;
+              const isLast = idx >= all.length - 1;
+              const { resizing = null } = image;
 
-                const resizeValue = get(resizing, 'value', 0);
+              const resizeValue = get(resizing, 'value', 0);
+              const resizeDir = get(resizing, 'direction', 'right');
 
-                return (
-                  <AreaSlot
-                    resizeValue={resizeValue}
-                    key={`template_slot_${this.currentTemplate.id}_${idx}`}>
-                    {(!isFirst || this.visibleImages.length === 1) && (
-                      <HandleLeft onMouseDown={this.onHandleMouseDown(image, 'left')} />
-                    )}
-                    {(!isLast || this.visibleImages.length === 1) && (
-                      <HandleRight onMouseDown={this.onHandleMouseDown(image, 'right')} />
-                    )}
-                    <Item dangerouslySetInnerHTML={{ __html: image.svg }} />
-                  </AreaSlot>
-                );
-              })}
+              const resizeStyle = {
+                left: resizeDir === 'left' && resizeValue > 0 ? `-${resizeValue}px` : 'auto',
+                right: resizeDir === 'left' && resizeValue < 0 ? `${resizeValue}px` : 'auto',
+                width: `calc(100% + ${resizeValue}px)`,
+              };
+
+              return (
+                <AreaSlot
+                  style={resizeStyle}
+                  resizeValue={resizeValue}
+                  key={`template_slot_${this.currentTemplate.id}_${idx}`}>
+                  {(!isFirst || image.size > 1) && (
+                    <HandleLeft onMouseDown={this.onHandleMouseDown(image, 'left')} />
+                  )}
+                  {(!isLast || image.size > 1) && (
+                    <HandleRight onMouseDown={this.onHandleMouseDown(image, 'right')} />
+                  )}
+                  <Item dangerouslySetInnerHTML={{ __html: image.svg }} />
+                </AreaSlot>
+              );
+            })}
           </Area>
         </AreaContainer>
       </Root>
