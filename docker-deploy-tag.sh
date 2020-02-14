9f4a257736a79eda8b048eb379249e9072caba52
Anton <anton.jyrkiainen@helsinki.fi>
2020-02-14 16:01:49 +0200

    tag changed

--- docker-deploy-tag.sh
+++ docker-deploy-tag.sh
@@ -8,7 +8,7 @@ read -p "Tag: " TAG
 
 DOCKER_TAG=${TAG}
 DOCKER_IMAGE=$ORG/hsl-map-publisher-ui:${DOCKER_TAG}
-DOCKER_IMAGE_CYPRESS=ahjyrkia/publisher-e2e:${DOCKER_TAG}
+DOCKER_IMAGE_CYPRESS=$ORG/publisher-ui-e2e:${DOCKER_TAG}
 
 docker build --build-arg BUILD_ENV=${TAG} -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
