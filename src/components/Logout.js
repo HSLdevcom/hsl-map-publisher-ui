0c2368c73811bd0df16a6cc4d755944789e52d2c
Anton <anton.jyrkiainen@helsinki.fi>
2020-07-07 16:53:08 +0300

    style improvements

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -7,23 +7,36 @@ const LoginWrapper = styled.div`
   width: 100%;
   white-space: nowrap;
   display: flex;
+  margin-bottom: 5px;
 `;
 
 const LogoutButton = styled.div`
-  display:flex;
+  display: flex;
   color: #0077c7;
   cursor: pointer;
   flex-direction: column;
   flex-grow: 0;
   text-align: right;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+  border-radius: 5px;
+  padding: 10px;
   :hover {
-    color: rgb(51, 51, 51);
+    border-radius: 5px;
+    background-color: #0077c7;
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1), 0 3px 14px rgba(0, 0, 0, 0.4);
+    cursor: pointer;
+    color: #fff;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+  }
 `;
 
 const Username = styled.div`
   display: flex;
   flex-grow: 1;
   flex-direction: column;
+  justify-content: center;
 `;
 
 class Logout extends Component {
