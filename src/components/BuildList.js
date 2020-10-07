a2c9f3cfd6e91eaba5ef55e13c852590f67386e7
Anton <anton.jyrkiainen@helsinki.fi>
2020-10-07 13:38:40 +0300

    show and hide older builds

--- src/components/BuildList.js
+++ src/components/BuildList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { inject, observer, PropTypes } from 'mobx-react';
 import styled from 'styled-components';
+import FlatButton from 'material-ui/FlatButton';
 
 import Build from './Build';
 
@@ -16,20 +17,72 @@ const Root = styled.div`
   overflow-y: auto;
 `;
 
-const BuildList = props => (
-  <Root>
-    {props.commonStore.builds.map(build => (
-      <Build
-        key={build.id}
-        {...build}
-        onStatusChange={value => props.commonStore.updateBuild({ id: build.id, status: value })}
-        onSelect={() => props.commonStore.showBuild(build.id)}
-        onRemoveBuild={() => props.commonStore.removeBuild(build.id)}
-        onRemovePoster={id => props.commonStore.removePoster(id)}
-      />
-    ))}
-  </Root>
-);
+const ToggleBuildsContainer = styled.div`
+  width: calc(100vw - 120px);
+  max-width: 880px;
+  margin-top: 10px;
+  white-space: nowrap;
+`;
+
+const Row = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+class BuildList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxBuilds: 50,
+      buildsToggled: false,
+    };
+  }
+
+  toggleBuilds = () => {
+    const buildsToggled = !this.state.buildsToggled;
+    this.setState({ buildsToggled });
+  };
+
+  toggleBuildsLabel = buildsAmount => {
+    const amount = buildsAmount - this.state.maxBuilds;
+    return this.state.buildsToggled ? `Piilota (${amount})` : `Näytä lisää (${amount})`;
+  };
+
+  render() {
+    const storeBuilds = this.props.commonStore.builds.filter(build => build !== undefined);
+    let builds = storeBuilds;
+    if (!this.state.buildsToggled) {
+      builds = storeBuilds.slice(0, this.state.maxBuilds);
+    }
+    return (
+      <Root>
+        {builds.map(build => (
+          <Build
+            key={build.id}
+            {...build}
+            onStatusChange={value =>
+              this.props.commonStore.updateBuild({ id: build.id, status: value })
+            }
+            onSelect={() => this.props.commonStore.showBuild(build.id)}
+            onRemoveBuild={() => this.props.commonStore.removeBuild(build.id)}
+            onRemovePoster={id => this.props.commonStore.removePoster(id)}
+          />
+        ))}
+        <ToggleBuildsContainer>
+          <Row>
+            <FlatButton
+              onClick={() => this.toggleBuilds()}
+              label={this.toggleBuildsLabel(storeBuilds.length)}
+              style={{ marginLeft: 10, color: 'rgb(0, 119, 199)' }}
+            />
+          </Row>
+        </ToggleBuildsContainer>
+      </Root>
+    );
+  }
+}
 
 BuildList.propTypes = {
   // eslint-disable-next-line react/no-typos
