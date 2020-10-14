a78a6d78a5bb72e81ea757812e3e0c338b3fe362
Anton <anton.jyrkiainen@helsinki.fi>
2020-10-14 18:57:24 +0300

    namespace info

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
 import styled from 'styled-components';
 import hslLogo from '../assets/hsl-logo.png';
 
@@ -16,7 +18,8 @@ const Wrapper = styled.div`
   position: fixed;
   top: 50%;
   left: 50%;
-  padding: 30px 90px;
+  width: 405px;
+  height: 275px;
   transform: translate(-50%, -50%);
   box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1), 0 3px 14px rgba(0, 0, 0, 0.4);
   border-radius: 2px;
@@ -54,6 +57,17 @@ const LoginButton = styled.span`
     box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.1), 0 3px 14px rgba(0, 0, 0, 0.4);
 `;
 
+const LightTooltip = withStyles(theme => ({
+  tooltip: {
+    backgroundColor: '#ffffffe6',
+    color: '#3e3e3e',
+    boxShadow: theme.shadows[1],
+    'font-family': 'Gotham Rounded SSm A, Gotham Rounded SSm B, Arial, Georgia, Serif',
+    'font-weight': 400,
+    fontSize: 11,
+  },
+}))(Tooltip);
+
 const LoginText = styled.span`
   margin-left: 10px;
 `;
@@ -88,6 +102,29 @@ const LoginIcon = () => (
   </svg>
 );
 
+const InfoIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="25"
+    height="25"
+    viewBox="0 0 172 172">
+    <g>
+      <path d="M0,172v-172h172v172z" fill="none" />
+      <g fill="#ffffff">
+        <path d="M86,6.88c-43.65603,0 -79.12,35.46397 -79.12,79.12c0,43.65603 35.46397,79.12 79.12,79.12c43.65603,0 79.12,-35.46397 79.12,-79.12c0,-43.65603 -35.46397,-79.12 -79.12,-79.12zM86,13.76c39.93779,0 72.24,32.30221 72.24,72.24c0,39.93779 -32.30221,72.24 -72.24,72.24c-39.93779,0 -72.24,-32.30221 -72.24,-72.24c0,-39.93779 32.30221,-72.24 72.24,-72.24zM86,37.84c-5.69958,0 -10.32,4.62042 -10.32,10.32c0,5.69958 4.62042,10.32 10.32,10.32c5.69958,0 10.32,-4.62042 10.32,-10.32c0,-5.69958 -4.62042,-10.32 -10.32,-10.32zM72.24,72.24v6.88h3.44h3.44v44.72h-3.44h-3.44v6.88h3.44h3.44h13.76h3.44h3.44v-6.88h-3.44h-3.44v-51.6h-3.44h-13.76z" />
+      </g>
+    </g>
+  </svg>
+);
+
+const InfoWrapper = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+`;
+
 class Login extends Component {
   openLoginForm = () => {
     window.location.replace(
@@ -99,16 +136,27 @@ class Login extends Component {
     return (
       <Root>
         <Wrapper>
+          <LightTooltip
+            title={
+              <React.Fragment>
+                Käytä nimiavaruuteen kirjattua tunnusta. <br />
+                {`Nimiavaruus: ${process.env.REACT_APP_NAMESPACE}`}
+              </React.Fragment>
+            }
+            placement="right-start">
+            <InfoWrapper>
+              <InfoIcon />
+            </InfoWrapper>
+          </LightTooltip>
+
           <Header>
             <Logo src={hslLogo} alt="HSL Logo" />
             <Title>HSL Karttajulkaisin</Title>
           </Header>
-          <p>
-            <LoginButton onClick={this.openLoginForm}>
-              <LoginIcon />
-              <LoginText>Kirjaudu (HSL ID)</LoginText>
-            </LoginButton>
-          </p>
+          <LoginButton onClick={this.openLoginForm}>
+            <LoginIcon />
+            <LoginText>Kirjaudu (HSL ID)</LoginText>
+          </LoginButton>
         </Wrapper>
       </Root>
     );
