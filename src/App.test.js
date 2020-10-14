bc8f48084bb379c53a25db6a3534ac7b45328f26
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-10-14 12:58:07 +0300

    Fix lint errors & tests (#36)

--- src/App.test.js
+++ src/App.test.js
@@ -4,51 +4,74 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { configure, shallow, mount } from 'enzyme';
 import { Provider } from 'mobx-react';
 import { Tabs } from 'material-ui/Tabs';
+import fetchMock from 'jest-fetch-mock';
+
 import App from './components/App';
 import Frame from './components/Frame';
 import theme from './components/theme';
 import ConfigureLayout from './components/ConfigureLayout';
-import stores from './stores/stores';
-
 import BuildDetails from './components/BuildDetails';
 import Generator from './components/Generator';
 
+import stores from './stores/stores';
+
 configure({ adapter: new Adapter() });
 
-it('Renders without crashing', () => {
-  shallow(<App />);
+fetchMock.enableMocks();
+
+describe('App component tests', () => {
+  it('App renders without crashing', () => {
+    shallow(<App />);
+  });
 });
 
-it('Frame renders without crashing', () => {
-  shallow(
+describe('Frame component tests', () => {
+  fetch.mockResponse(
+    JSON.stringify({
+      isOk: true,
+      email: 'testi@kayttaja.com',
+    }),
+  );
+
+  stores.commonStore.selectedBuild = {
+    id: '123456-65421',
+    title: 'testi',
+    status: 'OPEN',
+    posters: [],
+  };
+
+  const mountedWrapper = mount(
     <Provider {...stores}>
       <MuiThemeProvider muiTheme={theme}>
         <Frame />
       </MuiThemeProvider>
     </Provider>,
   );
-});
 
-const wrapper = mount(
-  <Provider {...stores}>
-    <MuiThemeProvider muiTheme={theme}>
-      <Frame />
-    </MuiThemeProvider>
-  </Provider>,
-);
+  it('Frame renders without crashing', () => {
+    shallow(
+      <Provider {...stores}>
+        <MuiThemeProvider muiTheme={theme}>
+          <Frame />
+        </MuiThemeProvider>
+      </Provider>,
+    );
+  });
 
-it('Frame contains BuildDetails', () => {
-  expect(wrapper.contains(BuildDetails)).toEqual(true);
-});
+  it('Frame contains BuildDetails', () => {
+    mountedWrapper.update();
+    expect(mountedWrapper.contains(BuildDetails)).toEqual(true);
+  });
 
-it('Frame contains Generator', () => {
-  expect(wrapper.contains(Generator)).toEqual(true);
-});
+  it('Frame contains Generator', () => {
+    expect(mountedWrapper.contains(Generator)).toEqual(true);
+  });
 
-it('Frame contains BuildDetails', () => {
-  expect(wrapper.contains(Tabs)).toEqual(true);
-});
+  it('Frame contains Tabs', () => {
+    expect(mountedWrapper.contains(Tabs)).toEqual(true);
+  });
 
-it('Frame contains ConfigureLayout', () => {
-  expect(wrapper.contains(ConfigureLayout)).toEqual(true);
+  it('Frame contains ConfigureLayout', () => {
+    expect(mountedWrapper.contains(ConfigureLayout)).toEqual(true);
+  });
 });
