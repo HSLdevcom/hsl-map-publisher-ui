8907473c89f60a3cc77dc228847a3c938aa7eaee
Petteri Pesonen <teemu.p.pesonen@gmail.com>
2020-10-20 14:35:19 +0300

    Add travis configuration for development & stage branches

--- docker-deploy-tag.sh
+++ docker-deploy-tag.sh
@@ -6,12 +6,14 @@ ORG=${ORG:-hsldevcom}
 
 read -p "Tag: " TAG
 
-DOCKER_TAG=${TAG}
+DOCKER_TAG=${TAG:-production}
 DOCKER_IMAGE=$ORG/hsl-map-publisher-ui:${DOCKER_TAG}
-DOCKER_IMAGE_CYPRESS=$ORG/publisher-ui-e2e:${DOCKER_TAG}
+DOCKER_IMAGE_CYPRESS=$ORG/publisher-ui-e2e:dev
 
-docker build --build-arg BUILD_ENV=${TAG} -t $DOCKER_IMAGE .
+docker build --build-arg BUILD_ENV=${TAG:-production} -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
 
-docker build --build-arg BUILD_ENV=${TAG} -t $DOCKER_IMAGE_CYPRESS -f e2e.dockerfile .
-docker push $DOCKER_IMAGE_CYPRESS
+if [[ $DOCKER_TAG == "dev" ]]; then
+  docker build --build-arg BUILD_ENV=dev -t $DOCKER_IMAGE_CYPRESS -f e2e.dockerfile .
+  docker push $DOCKER_IMAGE_CYPRESS
+fi
--- travis-build.sh
+++ travis-build.sh
@@ -3,15 +3,52 @@ set -e
 
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
-DOCKER_IMAGE=$ORG/hsl-map-publisher-ui:${DOCKER_TAG}
-DOCKER_IMAGE_LATEST=$ORG/hsl-map-publisher-ui:latest
+BUILD_ENV=production
+BUILD_AND_PUSH_BRANCHES=('development', 'stage', 'master')
 
-# Use latest while it may still be deployed to K5
-docker build --build-arg BUILD_ENV=latest --tag=$DOCKER_IMAGE .
+if [[ $TRAVIS_BRANCH == "development" ]]; then
+  DOCKER_TAG=dev
+  BUILD_ENV=dev
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
-  docker login -u $DOCKER_USER -p $DOCKER_AUTH
-  docker push $DOCKER_IMAGE
-  docker tag $DOCKER_IMAGE $DOCKER_IMAGE_LATEST
-  docker push $DOCKER_IMAGE_LATEST
+  echo "Building cypress test image with env -> dev and tag -> dev"
+
+  DOCKER_IMAGE_CYPRESS=$ORG/publisher-ui-e2e:dev
+  docker build --build-arg BUILD_ENV=dev -t $DOCKER_IMAGE_CYPRESS -f e2e.dockerfile .
+
+  if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+    echo "Pushing builded cypress test image to registry with tag -> dev"
+
+    docker login -u $DOCKER_USER -p $DOCKER_AUTH
+    docker push $DOCKER_IMAGE_CYPRESS
+  fi
 fi
+
+if [[ $TRAVIS_BRANCH == "stage" ]]; then
+  DOCKER_TAG=stage
+  BUILD_ENV=stage
+fi
+
+if [[ $TRAVIS_BRANCH == "master" ]]; then
+  DOCKER_TAG=production
+  BUILD_ENV=production
+fi
+
+DOCKER_IMAGE=$ORG/hsl-map-publisher-ui:${DOCKER_TAG}
+
+echo "Building image with env -> ${BUILD_ENV} and tag -> ${DOCKER_TAG}"
+
+docker build --build-arg BUILD_ENV=$BUILD_ENV --tag=$DOCKER_IMAGE .
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  if [[ " ${BUILD_AND_PUSH_BRANCHES[*]} " == *"$TRAVIS_BRANCH"* ]]; then
+    echo "Pushing builded image to registry with tag -> ${DOCKER_TAG}"
+
+    docker login -u $DOCKER_USER -p $DOCKER_AUTH
+    docker push $DOCKER_IMAGE
+  else
+    echo "Pushed branch is not targeted environment branch (development, stage, master). Image is not pushed to registry"
+  fi
+else
+  echo "Image is not pushed to registry for pull requests"
+fi
+
