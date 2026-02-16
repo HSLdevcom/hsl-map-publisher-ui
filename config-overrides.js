1b4456c491fcb7fcba60683baef9f7c20d81952a
Sebastian Szeszko <se.szeszko@gmail.com>
2026-02-16 08:08:57 +0100

    AB#68391 fix critical vulnerabilities

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,3 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const { override, addBabelPlugin } = require('customize-cra');
 
-module.exports = function(config, env) {
-  
-  // Essential features
-  config = injectBabelPlugin('transform-class-properties', config);
-  config = injectBabelPlugin('transform-decorators-legacy', config);
-  
-  // React Hot Loader
-  config = rewireReactHotLoader(config, env);
-  
-  return config;
-};
+module.exports = override(addBabelPlugin(['@babel/plugin-proposal-decorators', { legacy: true }]));
