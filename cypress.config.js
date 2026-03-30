dcdcb8bf6d09d816211ee8fe460a4620d24dd365
Avi <se.szeszko@gmail.com>
2026-03-30 08:46:51 +0200

    fix high and critical vulnerabilities (#158)

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const _ = require('lodash');
 const dotenv = require('dotenv');
-
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+const { defineConfig } = require('cypress');
 
 async function getConfigurationByFile(file) {
   if (!file) {
@@ -22,51 +10,62 @@ async function getConfigurationByFile(file) {
   }
 
   const pathToConfigFile = path.resolve(`cypress.${file}.json`);
+  if (!(await fs.pathExists(pathToConfigFile))) {
+    return {};
+  }
+
   return fs.readJson(pathToConfigFile);
 }
 
 async function readEnvVars() {
   const config = {};
-
   const appRoot = await fs.realpath(process.cwd());
   const envPaths = [path.resolve(appRoot, '.env')];
   const envObjects = [];
 
   for (const envPath of envPaths) {
-    const pathExists = await fs.exists(envPath);
-
-    if (!pathExists) {
+    if (!(await fs.pathExists(envPath))) {
       continue;
     }
-
     const envFile = await fs.readFile(envPath, 'utf8');
-    const envContent = dotenv.parse(envFile);
-    envObjects.push(envContent);
+    envObjects.push(dotenv.parse(envFile));
   }
 
   const combinedFiles = _.merge({}, ...envObjects);
-
   const CYPRESS_PREFIX = /^CYPRESS_/i;
 
-  // Add CYPRESS-prefixed vars to the config.
   Object.entries(combinedFiles).forEach(([key, value]) => {
     config[key] = value;
   });
 
-  // Add CYPRESS-prefixed vars from the environment to the config.
   for (const [envName, envValue] of Object.entries(process.env)) {
     if (envName.match(CYPRESS_PREFIX)) {
       config[envName] = envValue;
     }
   }
+
   return config;
 }
 
-module.exports = async (on, config) => {
+async function setupNodeEvents(on, config) {
   const configFile = config.env.configFile || '';
-
   const envVars = await readEnvVars();
   const envConfig = await getConfigurationByFile(configFile);
 
   return _.merge({}, config, { env: envVars }, envConfig);
-};
+}
+
+module.exports = defineConfig({
+  apiUrl: process.env.CYPRESS_API_URL || 'http://localhost:4000',
+  e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
+    numTestsKeptInMemory: 1,
+    projectId: '3o6x11',
+    specPattern: 'cypress/integration/**/*.spec.js',
+    supportFile: 'cypress/support/index.js',
+    setupNodeEvents,
+    viewportWidth: 800,
+    viewportHeight: 1100,
+    video: true,
+  },
+});
